15df1e25bee7917e08990b2be419eccab8c9d4a6
namanyash <naman_yash@yahoo.in>
2023-03-29 10:15:05 +1100

    logging added

--- routes/api/calculator.js
+++ routes/api/calculator.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+const winston = require("winston");
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "calculator-microservice" },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
 router.post(
   "/add",
   [
@@ -13,9 +27,17 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // Show error if validations failed
+      logger.log({
+        level: "error",
+        message: `Invalid Input: ${errors.array}`,
+      });
       return res.status(400).json({ errors: errors.array() });
     }
     const { a, b } = req.body;
+    logger.log({
+      level: "info",
+      message: `Add a=${a} b=${b}`,
+    });
     return res.status(200).send({ result: `${parseInt(a) + parseInt(b)}` });
   }
 );
@@ -30,9 +52,17 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // Show error if validations failed
+      logger.log({
+        level: "error",
+        message: `Invalid Input: ${errors.array}`,
+      });
       return res.status(400).json({ errors: errors.array() });
     }
     const { a, b } = req.body;
+    logger.log({
+      level: "info",
+      message: `Subtract a=${a} b=${b}`,
+    });
     return res.status(200).send({ result: `${parseInt(a) - parseInt(b)}` });
   }
 );
@@ -47,9 +77,17 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // Show error if validations failed
+      logger.log({
+        level: "error",
+        message: `Invalid Input: ${errors.array}`,
+      });
       return res.status(400).json({ errors: errors.array() });
     }
     const { a, b } = req.body;
+    logger.log({
+      level: "info",
+      message: `Divide a=${a} b=${b}`,
+    });
     return res.status(200).send({ result: `${parseInt(a) / parseInt(b)}` });
   }
 );
@@ -64,9 +102,17 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // Show error if validations failed
+      logger.log({
+        level: "error",
+        message: `Invalid Input: ${errors.array}`,
+      });
       return res.status(400).json({ errors: errors.array() });
     }
     const { a, b } = req.body;
+    logger.log({
+      level: "info",
+      message: `Multiply a=${a} b=${b}`,
+    });
     return res.status(200).send({ result: `${parseInt(a) * parseInt(b)}` });
   }
 );
